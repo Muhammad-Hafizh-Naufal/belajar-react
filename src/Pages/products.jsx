53f8e8668276413fdae7154ac6d257620b028841
@Hafizh_Naufal <noval1122567@gmail.com>
2024-09-15 22:17:04 +0700

    fix style card

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -4,18 +4,17 @@ export default function ProductsPage() {
   return (
     <>
       <div className="flex justify-center py-5">
-        <CardProduct.CardHeader image="images/MALM.webp" alt="Lemari">
-          {" "}
-        </CardProduct.CardHeader>
-        <CardProduct.CardBody title="Lemari Minimalis">
-          {" "}
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
-          distinctio velit sed facere nobis soluta eaque, numquam explicabo
-          tempore dolor fugiat sint molestias, quidem deleniti iste itaque aut
-          incidunt qui!
-        </CardProduct.CardBody>
-
-        <CardProduct.CardFooter price="200.000" />
+        <CardProduct>
+          <CardProduct.CardHeader alt="MALM" image="/images/MALM.webp" />
+          <CardProduct.CardBody title=" Lemarai Estetik">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
+            quibusdam magnam optio. Laborum ex, impedit, quam veniam minus
+            cupiditate praesentium voluptates eligendi minima mollitia ad
+            aliquid nulla repellendus culpa suscipit?{" "}
+          </CardProduct.CardBody>
+          <CardProduct.CardFooter price="999.999" />
+        </CardProduct>
       </div>
     </>
   );
--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -1,51 +1,40 @@
 import Button from "../Elements/Button/Index";
+
 const CardProduct = (props) => {
   const { children } = props;
   return (
-    <>
-      <div className="w-full max-w-sm bg-slate-100 border border-gray-200 rounded-lg shadow">
-        {children}
-      </div>
-    </>
+    <div className="w-full max-w-sm bg-slate-100 border border-gray-200 rounded-lg shadow">
+      {children}
+    </div>
   );
 };
 
 const CardHeader = (props) => {
   const { alt, image } = props;
   return (
-    <>
-      <a href="">
-        <img src={image} alt={alt} className="p-8 rounded-t-lg" />
-      </a>
-    </>
+    <a href="">
+      <img src={image} alt={alt} className="w-full rounded-t-lg" />
+    </a>
   );
 };
 
 const CardBody = (props) => {
   const { title, children } = props;
   return (
-    <>
-      <div className="px-5 pb-5">
-        <a href="">
-          <h5 className="text-xl font-semibold tracking-tight text-gray-900">
-            {title}
-          </h5>
-        </a>
-        <p className="text-gray-700 text-s">{children}</p>
-      </div>
-    </>
+    <div className="px-5 py-4">
+      <h5 className="text-xl font-bold mb-2">{title}</h5>
+      <p className="text-gray-700 text-base">{children}</p>
+    </div>
   );
 };
 
 const CardFooter = (props) => {
-  const { price = "0" } = props;
+  const { price } = props;
   return (
-    <>
-      <div className="flex items-center justify-between py-5 pb-5">
-        <span className="text-xl font-bold text-gray-900">Rp.{price}</span>
-        <Button className="bg-slate-600">Masukan Keranjang</Button>
-      </div>
-    </>
+    <div className="flex items-center justify-between py-5 px-6">
+      <span className="text-xl font-bold text-gray-900">Rp{price}</span>
+      <Button classname="bg-amber-500">Masukan Keranjang</Button>
+    </div>
   );
 };
 
